af09217eacc248d8cb47186cbbc699c6bec3f6bf
jemn21819 <jmartinez21734@stu.nuc.edu>
2022-03-03 13:46:52 +0000

    adding files

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,7 @@
+function welcome (firstname, lastname) {
+    let fullname = firstname + " " + lastname;
+    function displayFullName () {
+        alert("welcome "+fullname + "!");
+    }
+    displayFullName();
+}
